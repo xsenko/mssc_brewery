d2c8d3da19102c9dfe007b19b82a5d2176c20ec9
selcuk cabuk <cabuk.selcuk@gmail.com>
2020-12-05 02:05:51 +0100

    initial commit

---
+++ src/test/java/com/senko/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senko.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
