8518dc62175a41e4304f53cbcd0a99408c703bca
selcuk cabuk <cabuk.selcuk@gmail.com>
2020-12-05 18:23:34 +0100

    add POST for beer

--- src/main/java/com/senko/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/senko/msscbrewery/web/controller/BeerController.java
@@ -5,12 +5,10 @@ import com.senko.msscbrewery.services.CustomerService;
 import com.senko.msscbrewery.web.model.BeerDto;
 import com.senko.msscbrewery.web.model.CustomerDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -28,5 +26,16 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(BeerDto beerDto) {
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //todo add servername to url
+        headers.add("Location", "api/v1/beer" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 }
